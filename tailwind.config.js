ae639367e016dee2b8569a068cfb214a43d745f5
Milagros O. Almidon <orihuela.ms@gmail.com>
2022-09-27 20:34:31 -0500

    feat: add configuration for generate css

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+const themeHorizon = require('./settings/tailwind/horizon/galaxy');
+
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: themeHorizon,
+  plugins: [],
+};
